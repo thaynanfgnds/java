3d6eadafccfbbb9f939f7a2081b030bc1a80bbdb
pedro fagundes <thaynanfgds@gmail.com>
2023-07-31 15:24:52 -0300

    ternario

---
+++ src/fundamentos/Ternario.java
@@ -0,0 +1,16 @@
+package fundamentos;
+
+public class Ternario {
+
+	public static void main(String[] args) {
+		
+		double media = 8.6;
+		String resultadoFinal = media >= 7.0 ? 
+				"aprovado ." : "em recuperaçãp.";
+		System.out.println("O aluno está " + resultadoFinal);
+		
+		
+		
+		
+	}
+}
