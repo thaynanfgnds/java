439bda0e282398f163391678d4834d79a1a4a874
pedro fagundes <thaynanfgds@gmail.com>
2023-07-31 15:18:13 -0300

    unarios

--- src/fundamentos/Relacionais.java
+++ src/fundamentos/Relacionais.java
@@ -7,7 +7,6 @@ public class Relacionais {
 		int a = 97;
 		int b = 'a';
 		System.out.println(a == b);
-		
 		System.out.println(3 > 4);
 		System.out.println(3 >= 3);
 		System.out.println(3 < 7);
