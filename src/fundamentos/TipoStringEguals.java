5efc6240f140531389113e1c327afe0e1a8c8a20
pedro fagundes <thaynanfgds@gmail.com>
2023-07-31 17:31:10 -0300

    tipostringeguals

---
+++ src/fundamentos/TipoStringEguals.java
@@ -0,0 +1,14 @@
+package fundamentos;
+
+public class TipoStringEguals {
+
+	
+	public static void main(String[] args) {
+		
+		System.out.println("2" == "2");
+		
+		String s1 = new String("2");
+		System.out.println("2" == s1);
+		System.out.println("2".equals(s1));
+	}
+}
